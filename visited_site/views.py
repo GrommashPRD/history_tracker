7b1b9b349e3cd54ab1b0b6139c1d94290cd2973d
grommashPRD <epavel425@gmail.com>
2025-01-27 12:26:27 +0300

    Убрал старый код

--- visited_site/views.py
+++ visited_site/views.py
@@ -21,7 +21,7 @@ class VisitedLinksView(APIView):
 
         for url in urls:
             parsed_url = urlparse(url)
-            if parsed_url.scheme and parsed_url.netloc:  # Проверяем, что URL валиден
+            if parsed_url.scheme and parsed_url.netloc:
                 VisitedLink.objects.get_or_create(url=url)
 
         return Response({'status': 'ok'}, status=status.HTTP_201_CREATED)
@@ -47,89 +47,3 @@ class VisitedLinksView(APIView):
 
         return Response({"domains": list(unique_domains), "status": "ok"}, status=status.HTTP_200_OK)
 
-
-
-
-
-
-
-
-
-
-
-# class VisitedLinks(ModelViewSet):
-#     queryset = Visited.objects.all()
-#     serializer_class = VisitLink
-#
-#     def post(self, request, *args, **kwargs):
-#         url = request.data.get['url', []]
-#
-#         if isinstance(url, list):
-#             for link in url:
-#                 Visited.objects.create(url=link)
-#
-#         return Response({"status": "OK"}, status=status.HTTP_201_CREATED)
-#
-
-
-# class VisitedLinks(ModelViewSet):
-#     queryset = Visited.objects.all()
-#     serializer_class = VisitLink
-#
-#     def create(self, request, *args, **kwargs):
-#         worker_id = request.data.get('worker_id')
-#         url = request.data.get('url', [])
-#
-#         if isinstance(url, list):
-#             for link in url:
-#                 Visited.objects.get_or_create(worker_id=worker_id, url=link)
-#
-#         if not worker_id or not url:
-#             return Response({'error': 'ID сотрудника не найден'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#         try:
-#             worker = Worker.objects.get(id=worker_id)
-#             visited_link = Visited.objects.create(worker=worker, url=url)
-#             serializer = self.get_serializer(visited_link)
-#             return Response({"status": "OK"}, status=status.HTTP_201_CREATED)
-#
-#         except Worker.DoesNotExist:
-#             return Response({'error': 'Сотрудник не найден.'}, status=status.HTTP_404_NOT_FOUND)
-
-
-    # def get(self, request):
-    #     all_links = Visited.objects.all()
-    #     serializer = self.get_serializer(all_links, many=True)
-    #
-    #     unique_urls = list(set(link['url'] for link in serializer.data))
-    #
-    #     return Response({"links": unique_urls, "status": "OK"}, status=status.HTTP_200_OK)
-
-
-
-
-
-# class VisitedLinks(ModelViewSet):
-#     queryset = Visited.objects.all()
-#     serializer_class = VisitLink
-#
-#     def create(self, request, *args, **kwargs):
-#         worker_id = request.data.get('worker_id')
-#         url = request.data.get('url', [])
-#
-#         if not worker_id or not url:
-#             return Response({'error': 'ID сотрудника не найден'}, status=status.HTTP_400_BAD_REQUEST)
-#
-#         try:
-#             worker = Worker.objects.get(id=worker_id)
-#             visited_link = Visited.objects.create(worker=worker, url=url)
-#             serializer = self.get_serializer(visited_link)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#
-#         except Worker.DoesNotExist:
-#             return Response({'error': 'Сотрудник не найден.'}, status=status.HTTP_404_NOT_FOUND)
-
-
-# class WorkerViewSet(ModelViewSet):
-#     queryset = Worker.objects.all()
-#     serializer_class = WorkerDomains
